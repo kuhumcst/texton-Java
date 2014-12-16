5b4b0d892aec7e6cff4a6d9235b17a78c79cd357
Bart <bartj@hum.ku.dk>
2014-12-16 15:39:00 +0100

    CATALINA_HOME for lib, CATALINA_BASE for webapp

--- compileTomcat.sh
+++ compileTomcat.sh
@@ -1,4 +1,7 @@
-set CATALINA_BASE=/var/lib/tomcat7
-call ant -Divy=true download-ivy 
-call ant -DCATALINA_BASE=$CATALINA_BASE -Dtomcat=true war
-sudo cp war/tools.war %CATALINA_BASE%/webapps
+CATALINA_BASE=/var/lib/tomcat7
+CATALINA_HOME=/usr/share/tomcat7
+export CATALINA_BASE
+export CATALINA_HOME_
+ant -Divy=true download-ivy 
+ant -DCATALINA_HOME=$CATALINA_HOME -Dtomcat=true war
+sudo cp war/tools.war $CATALINA_BASE/webapps
