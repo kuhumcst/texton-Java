bbdd7c657a2794bde86bdc42277ca0bf5c7a0e68
Bart <bartj@hum.ku.dk>
2014-12-17 15:51:13 +0100

    Made it almost work. Writing to disk doesn't work. File rights?

--- compileTomcat.sh
+++ compileTomcat.sh
@@ -3,5 +3,5 @@ CATALINA_HOME=/usr/share/tomcat7
 export CATALINA_BASE
 export CATALINA_HOME_
 ant -Divy=true download-ivy 
-ant -DCATALINA_HOME=$CATALINA_HOME -Dtomcat=true war
+ant -DCATALINA_HOME=$CATALINA_HOME -DtomcatU=true war
 sudo cp war/tools.war $CATALINA_BASE/webapps
