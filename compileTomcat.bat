089a7fc62b20dcb5e8a7126137da3c54c0aca484
Bart Jongejan <zgk261@ku.dk>
2019-06-19 18:13:16 +0200

    Updated environment variables for jdk 12 and tomcat 9

--- compileTomcat.bat
+++ compileTomcat.bat
@@ -1,6 +1,7 @@
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_31
-set CATALINA_BASE="C:\Program Files\Apache Software Foundation\Tomcat 8.5"
-set CATALINA_HOME=C:\Program Files\Apache Software Foundation\Tomcat 8.5
+set JAVA_HOME=C:\Program Files\Java\jdk-12.0.1
+set ANT_HOME=C:\apache-ant-1.10.6
+set CATALINA_BASE="C:\Program Files\Apache Software Foundation\Tomcat 9.0"
+set CATALINA_HOME=C:\Program Files\Apache Software Foundation\Tomcat 9.0
 call ant -Divy=true download-ivy 
 call ant -DCATALINA_BASE=%CATALINA_BASE% -Dtomcat=true war
 copy war\tools.war %CATALINA_BASE%\webapps
