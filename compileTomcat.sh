d889e4f61c8f20a88d50f54cd6bfd44df4ca91d1
Bart Jongejan <zgk261@ku.dk>
2019-06-19 18:14:42 +0200

    CATALINA_HOME and _BASE in two scenarios

--- compileTomcat.sh
+++ compileTomcat.sh
@@ -1,5 +1,10 @@
-CATALINA_BASE=/var/lib/tomcat7
-CATALINA_HOME=/usr/share/tomcat7
+if [ -d /usr/share/tomcat9/lib ]; then
+    CATALINA_BASE=/var/lib/tomcat9
+    CATALINA_HOME=/usr/share/tomcat9
+elif  [ -d /opt/tomcat/latest/lib ]; then
+    CATALINA_BASE=/opt/tomcat/latest
+    CATALINA_HOME=/opt/tomcat/latest
+fi
 export CATALINA_BASE
 export CATALINA_HOME_
 ant -Divy=true download-ivy 
