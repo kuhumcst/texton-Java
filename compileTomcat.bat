d7995a441a8d65aeba8ce4adf3769ee2824f506e
Bart Jongejan <zgk261@ku.dk>
2019-06-26 11:16:01 +0200

    Changed -Dtomcat to -D windows and -DtomcatU to -DUbuntu

--- compileTomcat.bat
+++ compileTomcat.bat
@@ -3,5 +3,5 @@ set ANT_HOME=C:\apache-ant-1.10.6
 set CATALINA_BASE="C:\Program Files\Apache Software Foundation\Tomcat 9.0"
 set CATALINA_HOME=C:\Program Files\Apache Software Foundation\Tomcat 9.0
 call ant -Divy=true download-ivy 
-call ant -DCATALINA_BASE=%CATALINA_BASE% -Dtomcat=true war
+call ant -DCATALINA_BASE=%CATALINA_BASE% -Dwindows=true war
 copy war\texton.war %CATALINA_BASE%\webapps
--- compileTomcat.sh
+++ compileTomcat.sh
@@ -8,5 +8,5 @@ fi
 export CATALINA_BASE
 export CATALINA_HOME_
 ant -Divy=true download-ivy 
-ant -DCATALINA_HOME=$CATALINA_HOME -DtomcatU=true war
+ant -DCATALINA_HOME=$CATALINA_HOME -DUbuntu=true war
 sudo cp war/texton.war $CATALINA_BASE/webapps
