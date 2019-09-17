cd596d3ed323ea7e4c3c1ef206b1f0ba8c8822b8
Bart Jongejan <bartj@hum.ku.dk>
2019-09-17 18:11:22 +0200

    Updated path to ant

--- compileTomcat.bat
+++ compileTomcat.bat
@@ -1,5 +1,5 @@
-set JAVA_HOME=C:\Program Files\Java\jdk-12.0.1
-set ANT_HOME=C:\apache-ant-1.10.6
+set JAVA_HOME=C:\Program Files\Java\jdk-12.0.2
+set ANT_HOME=C:\apache-ant-1.10.7
 set CATALINA_BASE="C:\Program Files\Apache Software Foundation\Tomcat 9.0"
 set CATALINA_HOME=C:\Program Files\Apache Software Foundation\Tomcat 9.0
 call ant -Divy=true download-ivy 
