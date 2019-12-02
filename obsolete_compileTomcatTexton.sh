4ab8dea63e2c24bed4187e8ebb65a7b0ea891282
root <root@nlpweb01fl.unicph.domain>
2019-12-02 16:49:32 +0000

    Clean up ignored files

---
+++ obsolete_compileTomcatTexton.sh
@@ -0,0 +1,11 @@
+if [ -d /usr/share/tomcat9/lib ]; then
+    CATALINA_BASE=/var/lib/tomcat9
+    CATALINA_HOME=/usr/share/tomcat9
+elif  [ -d /opt/tomcat/latest/lib ]; then
+    CATALINA_BASE=/opt/tomcat/latest
+    CATALINA_HOME=/opt/tomcat/latest
+fi
+export CATALINA_BASE
+export CATALINA_HOME_
+ant -Divy=true download-ivy 
+ant -DCATALINA_HOME=$CATALINA_HOME -Dtexton=true war
---
+++ work/data/readme/readme.txt
@@ -0,0 +1,3 @@
+Folder work/data is for input, intermediate results and final results.
+Run the cleanup service (e.g. http://localhost/texton/cleanup) to thin out.
+You can in addition set up a cron job that deletes files over e.g. 30 days old.
