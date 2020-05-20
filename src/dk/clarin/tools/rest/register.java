1bddb972b9f775fd21aa1c42c8b6a80a24689740
Bart Jongejan <bart@cst.dk>
2020-05-20 17:06:34 +0200

    Added 'charset=' in contenttype

--- src/dk/clarin/tools/rest/register.java
+++ src/dk/clarin/tools/rest/register.java
@@ -141,7 +141,7 @@ public class register extends HttpServlet
             else if(result.startsWith("<?\nheader") || arg.contains("PHP"))
                 { /* php wrapper */
                 //response.setContentType("text/plain; charset=iso8859-1");//UTF-8");
-                response.setContentType("text/plain; UTF-8");
+                response.setContentType("text/plain; charset=UTF-8");
                 response.setStatus(200);
                 out.println(result);
                 }
