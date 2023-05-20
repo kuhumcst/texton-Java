f02c2c92542a75a1a7be50d6ea1142a5a81d1774
Bart Jongejan <bart.jongejan@gmail.com>
2023-05-20 15:29:44 +0200

    Page with contact details and feedback text box

---
+++ src/dk/clarin/tools/rest/contact.java
@@ -0,0 +1,92 @@
+/*
+    Copyright 2014, Bart Jongejan
+    This file is part of the DK-ClarinTools.
+
+    DK-ClarinTools is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DK-ClarinTools is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with DK-ClarinTools.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package dk.clarin.tools.rest;
+
+import dk.clarin.tools.ToolsProperties;
+import dk.clarin.tools.util;
+import dk.cst.bracmat;
+
+import jakarta.servlet.ServletConfig; 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+
+import java.security.AccessControlException;
+
+import java.util.Enumeration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SuppressWarnings("serial")
+@MultipartConfig(fileSizeThreshold=1024*1024*10,  // 10 MB 
+                 maxFileSize=-1/*1024*1024*50*/,       // 50 MB
+                 maxRequestSize=-1/*1024*1024*100*/)    // 100 MB
+
+public class contact extends HttpServlet 
+    {
+    private static final Logger logger = LoggerFactory.getLogger(contact.class);
+
+    private bracmat BracMat;
+
+    public void init(ServletConfig config) throws ServletException 
+        {
+        InputStream fis = config.getServletContext().getResourceAsStream("/WEB-INF/classes/properties.xml");
+        ToolsProperties.readProperties(fis);        
+        BracMat = new bracmat(ToolsProperties.bootBracmat);
+        super.init(config);
+        }
+
+    public void doGet(HttpServletRequest request,HttpServletResponse response)
+        throws ServletException, IOException 
+        {
+        logger.info("Calling texton/contact");
+        response.setContentType("text/html; charset=UTF-8");
+        response.setStatus(200);
+        if(!BracMat.loaded())
+            {
+            response.setStatus(500);
+            throw new ServletException("Bracmat is not loaded. Reason:" + BracMat.reason());
+            }
+        /*Test:*/
+        @SuppressWarnings("unchecked")
+        Enumeration<String> parmNames = (Enumeration<String>)request.getParameterNames();
+        String arg = "";
+        for (Enumeration<String> e = parmNames ; e.hasMoreElements() ;) 
+            {
+            String parmName = e.nextElement();
+            String vals[] = request.getParameterValues(parmName);
+            for(int j = 0;j < vals.length;++j)
+                {
+                arg = "(" + util.quote(parmName) + "." + util.quote(vals[j]) + ") " + arg;
+                }
+            }
+
+        PrintWriter out = response.getWriter();
+        String svar = BracMat.Eval("contact$(" + arg + ")");
+        out.println(svar);
+        }
+    }
+
