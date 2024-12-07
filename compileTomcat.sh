b02f7e39fe648c257167ceb1a457fa11ea22b819
Bart Jongejan <bartj@hum.ku.dk>
2024-12-07 19:51:17 +0100

    Update compileTomcat.sh

--- compileTomcat.sh
+++ compileTomcat.sh
@@ -1,4 +1,13 @@
-if [ -d /opt/tomcat-texton/lib ]; then
+if [ -d /opt/tomcat11/latest/lib ]; then
+    CATALINA_BASE=/opt/tomcat11/latest
+    CATALINA_HOME=/opt/tomcat11/latest
+elif [ -d /opt/tomcat10/latest/lib ]; then
+    CATALINA_BASE=/opt/tomcat10/latest
+    CATALINA_HOME=/opt/tomcat10/latest
+elif [ -d /opt/tomcat/latest/lib ]; then
+    CATALINA_BASE=/opt/tomcat/latest
+    CATALINA_HOME=/opt/tomcat/latest
+elif [ -d /opt/tomcat-texton/lib ]; then
     CATALINA_BASE=/opt/tomcat-texton
     CATALINA_HOME=/opt/tomcat-texton
 fi
