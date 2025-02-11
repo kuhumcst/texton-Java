ee525693107cc8d2e235b5087b2106aaec269d6d
Bart Jongejan <bart.jongejan@gmail.com>
2025-02-11 16:15:02 +0100

    Not used anymore

--- src/dk/clarin/tools/rest/setLanguage.java
+++ src/dk/clarin/tools/rest/setLanguage.java
@@ -15,6 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with DK-ClarinTools.  If not, see <http://www.gnu.org/licenses/>.
 */
+/* NOT USED !!! */
 package dk.clarin.tools.rest;
 
 import dk.clarin.tools.ToolsProperties;
